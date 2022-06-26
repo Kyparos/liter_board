f2d9fc221b35f8b9c7b37a2b47f87f72bf366cdc
Kyparos <sasha.malakhatka@gmail.com>
2022-06-27 00:17:43 +0200

    likes fix working

--- board/urls.py
+++ board/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.register_request, name='register'),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("django.contrib.auth.urls")),
+    path('<int:user_id>/<int:post_id>/', views.set_clink, name='set_clink'),
 ]
