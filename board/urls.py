0f88524d6f0439034601469853ec1b68ecee1fb1
Kyparos <sasha.malakhatka@gmail.com>
2022-06-26 15:55:43 +0200

    fix

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'board'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_id>/', views.post_user, name='user_post')
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Post, CocktailRating, UserFollowing
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'board/index.html')
+
+
+def posts(request, user_id):
+    return render(request, 'board/posts.html', {'user': user_id})
+
+
+def post_user(request, user_id):
+    user_posts = User.objects.get(id=user_id)
+    return render(request, 'posts.html', {'user': user_posts,
+                                                'posts': Post.objects.filter(user=user_id),
+                                                'cocktail_rating': CocktailRating,
+                                                'followings': UserFollowing.objects.filter(user_id=user_id)})
