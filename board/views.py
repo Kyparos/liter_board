5723e5f545e95ca3cb2d757fb6c8a796ae367faa
Kyparos <sasha.malakhatka@gmail.com>
2022-06-26 21:56:53 +0200

    registration

--- board/views.py
+++ board/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from django.contrib.auth.models import User
 from .models import Post, CocktailRating, UserFollowing
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 def index(request):
@@ -16,9 +19,22 @@ def post_user(request, user_id):
     current_user = request.user
     user_posts = User.objects.get(id=user_id)
     posts = Post.objects.filter(user=user_id)
-    posts_list = list(map(lambda x: ( x, zip(x.cocktails.all(), x.cocktail_rating.all())), posts))
+    posts_list = list(map(lambda x: (x, zip(x.cocktails.all(), x.cocktail_rating.all())), posts))
     return render(request, 'posts.html', {'user': user_posts,
                                           'current_user': current_user,
-                                                'posts': posts_list,
-                                                'cocktail_rating': CocktailRating,
-                                                'followings': UserFollowing.objects.filter(user_id=user_id)})
+                                          'posts': posts_list,
+                                          'cocktail_rating': CocktailRating,
+                                          'followings': UserFollowing.objects.filter(user_id=user_id)})
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("board:index")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="register.html", context={"register_form": form})
