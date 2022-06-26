cd583cd87097babbe72046ddd2e7a8e0634e7c73
Kyparos <sasha.malakhatka@gmail.com>
2022-06-26 17:24:42 +0200

    stylies working 2

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, CocktailRating, UserFollowing, Ingredient, Image
+
+
 
 admin.site.register(Post)
-
+admin.site.register(CocktailRating)
+admin.site.register(UserFollowing)
+admin.site.register(Ingredient)
+admin.site.register(Image)
